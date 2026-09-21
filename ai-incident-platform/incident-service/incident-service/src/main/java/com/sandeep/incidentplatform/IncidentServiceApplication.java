6b9ad86dbfe4e3dc669c28f4b1954d93c5c2b628
Sandeep Konduru Raju <sandeepkraju1999@gmail.com>
2026-09-21 19:23:05 +0100

    Findby id Chnages

--- ai-incident-platform/incident-service/incident-service/src/main/java/com/sandeep/incidentplatform/IncidentServiceApplication.java
+++ ai-incident-platform/incident-service/incident-service/src/main/java/com/sandeep/incidentplatform/IncidentServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IncidentServiceApplication {
 
-	public static void main(String[] args) {
+	public static void  main(String[] args) {
 		SpringApplication.run(IncidentServiceApplication.class, args);
 	}
 
